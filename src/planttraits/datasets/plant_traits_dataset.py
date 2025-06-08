639caa71406889216bbb42b9dc972147775302d0
woznin <szymonw037@gmail.com>
2025-06-08 20:27:56 +0200

    add changes to plant traits dataset and image preprocessing

--- src/planttraits/datasets/plant_traits_dataset.py
+++ src/planttraits/datasets/plant_traits_dataset.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import torch
 
@@ -26,30 +28,33 @@ class PlantTraitsDataset(Dataset):
         self._preprocessors = {} if preprocessors is None else preprocessors
         self.is_train = preprocessors is None
         self.data = pd.read_csv(TRAIN_CSV_FILE if self.is_train else TEST_CSV_FILE, index_col='id')
+
         self.imgs_folder = TRAIN_IMAGES_FOLDER if self.is_train else TEST_IMAGES_FOLDER
+        self.imgs_paths = {int(p.split('.')[0]): self.imgs_folder / p for p in os.listdir(self.imgs_folder)}
 
         if self.is_train:
             # Tutaj też implicitly dokona się prepare_data i fit_preprocessing
             self._preprocessors = {
-                'img': ImagePreprocessing(
-                    self.imgs_folder
-                ),  # Tutaj nie nie chciałem ingerować za bardzo w zdjęcia, więc przekazuję imgs_folder
                 'modis_vod': ModisVodPreprocessing(self.data),  # tutaj im przekazuje data, to będzie train zawsze
                 'soil': SoilPreprocessing(self.data),
                 'worldclimbio': WorldClimBioPreprocessing(self.data),
                 'mean': MeanPreprocessing(self.data),
             }
+            self.img_preprocessor = ImagePreprocessing(
+                self.imgs_paths
+            )
         else:
             for key, preprocessor in self._preprocessors.items():
-                if key == 'img':
-                    preprocessor.prepare_data(self.imgs_folder)
-                else:
+                if key not in ['img', 'mean']:
                     preprocessor.prepare_data(self.data).transform_preprocessing(
                         self.data
                     )  # Tutaj też przekazuje data, ale to będzie typowo test i tylko prepare
-
+            self.img_preprocessor = ImagePreprocessing(
+                self.imgs_paths, is_train=False
+            )
     def return_preprocessors(self):
         return self._preprocessors
+    
 
     def __len__(self):
         return len(self.data)
@@ -57,7 +62,8 @@ class PlantTraitsDataset(Dataset):
     def __getitem__(self, idx):
         true_idx = self.data.index[idx]
         row = self.data.loc[true_idx]
-        img = self._preprocessors['img'].transform(true_idx)
+        img = self.img_preprocessor.transform(self.imgs_paths[true_idx])
+        # transformacje są wykonane w miejscu na danych więc tylko trzeba odpowiednie kolumny wybrać
         modisvod_row = self._preprocessors['modis_vod'].select(row)
         soil_row = self._preprocessors['soil'].select(row)
         worldclimbio_row = self._preprocessors['worldclimbio'].select(row)
@@ -66,3 +72,6 @@ class PlantTraitsDataset(Dataset):
         )
 
         return img, modisvod_row, soil_row, worldclimbio_row, std_row, mean_row
+
+    def transform_predictions(self, preds: torch.Tensor) -> torch.Tensor:
+        return self._preprocessors['mean'].reverse_transform(preds)
--- src/planttraits/preprocessing/img_preprocessing.py
+++ src/planttraits/preprocessing/img_preprocessing.py
@@ -1,71 +1,34 @@
-import os
-
 import torch
-
 from torchvision import transforms
-from planttraits.utils import DTYPE
 from torchvision.io import read_image
-
+from planttraits.utils import DTYPE
 
 class ImagePreprocessing:
-    def __init__(
-        self,
-        imgs_paths,  # add more parameters if necessary
-    ):
-        # zdjęcia treningowe
+    def __init__(self, imgs_paths, is_train = True):
         self.imgs_paths = imgs_paths
-        # To będa wczytywane zawsze treningowe.
         self._fit_preprocessing()
-
-    def _fit_preprocessing(
-        self,
-    ):  # Tylko treningowe, zastosowanie fit i scalera i wyliczanie wartosci tylko raz
-        """
-        Calculate parameters (e.g., mean, variance, coding maps, PCA matrix, etc.)
-        that are needed for subsequent data transformation.
-
-        Computation of statistics on cleaned data ex. avg, std_dv for scaler.
-        Teaching coder for categorical variables. (we don't have any I think)
-        Computation of reduction dimensionality parameters which are learning on data distribution.
-        """
-
-
-        image = image / 255.0
-
-        transform_list = []
-
-        transform_list += [
+        self.is_train = is_train
+        
+    def _fit_preprocessing(self):
+        self.train_transform = transforms.Compose([
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=1.0),
             transforms.RandomRotation(degrees=15),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        ]
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)
+        ])
 
-        transform_list.append(transforms.ConvertImageDtype(DTYPE))  # Ensure correct dtype
-
-        transform_pipeline = transforms.Compose(transform_list)
-
-        image = transform_pipeline(image)
-        pass
-
-    def _transform_preprocessing(self, image):  # Wspólny dla testowych i treningowych
-        # quick rescaling for testing purposes
-        image = image / 255.0
-        """
-        The use of common transformations, as well as the use of previously learned parameters
-         to process a single line (sample).
-
-        Perform type conversion (e.g., convert values from Pandas Series to floating-point numbers).
-        Normalize or scale the data using parameters learned earlier on the training set.
-        Extract or calculate additional features from existing data.
-        Apply possible augmentation operations (if working with images and want to introduce random
-        transformations for the training data).
-        """
+        self.test_transform = transforms.Compose([
+            transforms.ConvertImageDtype(DTYPE)
+        ])
 
 
-        return torch.tensor(image, dtype=DTYPE) 
+    def _transform_preprocessing(self, img_path):
+        image = read_image(img_path)
+        image = image.float() / 255.0
+        if self.is_train:
+            image = self.train_transform(image)
+        image = self.test_transform(image)
+        return image
 
     def transform(self, img_path) -> torch.Tensor:
-        image = read_image(img_path)
-        image = self._transform_preprocessing(image)  
-        return image
+        return self._transform_preprocessing(img_path)
